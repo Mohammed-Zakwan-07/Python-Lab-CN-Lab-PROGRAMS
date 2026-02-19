40742182716db4f71b7c38dad6469a6445538ec0
Mohammed Zakwan <zakwanmd16@gmail.com>
2026-02-19 07:20:39 +0530

    Create 14test_package.py
    Create a package with two modules
    
    my_math_package/
    ├── __init__.py
    ├── basic.py
    └── advanced.py

---
+++ 14test_package.py
@@ -0,0 +1,7 @@
+from my_math_package.basic import add, divide
+from my_math_package.advanced import factorial, square_root
+
+print(add(10, 25))          # 35
+print(divide(100, 4))       # 25.0
+print(square_root(144))     # 12.0
+print(factorial(6))         # 720
